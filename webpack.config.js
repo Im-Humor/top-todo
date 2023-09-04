a06213d858bc296d3f46ff7f3e36fbb42776c805
Im-Humor <orangite@gmail.com>
2023-09-04 09:11:50 -0700

    initialize webpack setup with html template file

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
